f13649f364585b581cf8f645ef9143fb76d5f55b
Reese Hartnett <juliapoole44@gmail.com>
2022-03-17 18:41:19 -0400

    fix schema and seeds files

--- schema.sql
+++ schema.sql
@@ -12,16 +12,18 @@ CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     role_name VARCHAR(30),
     salary DECIMAL,
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
     ON DELETE SET NULL
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee_info (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    FOREIGN KEY (role_id) REFERENCES roles(id)
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee_info(id)
     ON DELETE SET NULL
 );
--- seeds.sql
+++ seeds.sql
@@ -5,22 +5,23 @@ VALUES ("Engineering"),
        ("Sales");
 
 INSERT INTO roles (department_id, role_name, salary)
-VALUES (1, "Senior Engineer", 120000),
+VALUES (3, "Director", 160000),
+       (1, "Senior Engineer", 120000),
        (1, "Junior Engineer", 90000),
-       (2, "Finance Director", 100000),
        (2, "Accountant", 100000),
+       (2, "Finance Director", 100000),
        (3, "Legal Administrative Manager", 130000),
        (3, "Legal Administrative Assistant", 110000),
        (4, "Sales Manager", 105000),
        (4, "Advertising Manager", 95000);
 
--- INSERT INTO employees (role_id, first_name, last_name, manager_id)
--- VALUES (1, "Senior Engineer", 120000),
---        (1, "Junior Engineer", 90000),
---        (2, "Finance Director", 100000),
---        (2, "Accountant", 100000),
---        (3, "Legal Administrative Manager", 130000),
---        (3, "Legal Administrative Assistant", 110000),
---        (4, "Sales Manager", 105000),
---        (4, "Advertising Manager", 95000);
-       
+INSERT INTO employee_info (first_name, last_name, role_id, manager_id)
+VALUES ("Gilbert", "Chesterton", 1, 1),
+       ("Agatha", "Christie", 2, 1),
+       ("Kurt", "Vonnegut", 3, 2),
+       ("Neil", "Gaiman", 5, 1),
+       ("Terry", "Pratchett", 6, 1),
+       ("Richard", "Adams", 8, 1),
+       ("Douglas", "Adams", 4, 5),
+       ("Mary", "Shelly", 7, 6),
+       ("Isaac", "Asimov", 9, 8);
